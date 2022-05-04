1ca2ece475149b0be9c93d083070029c4a4547e1
Denis Truong <truongd@ku.edu>
2022-05-03 21:22:21 -0500

    built the data needed, combining section name with its table data

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,12 +1,11 @@
 import { createRoot } from 'react-dom/client';
 import React from 'react';
 import './styles/index.scss';
-
 import Categories from './categories.jsx';
 import ArticleRow from './articles.jsx';
 
 const root = createRoot(document.getElementById('root'));
-
+console.log()
 function App() {
   return (
     <div className="main-container">
